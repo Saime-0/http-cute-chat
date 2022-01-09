aa4c2520f05d2e1270d5e48373c60b8a11bb0752
Saime Dejuwyu <acc.saime.d@gmail.com>
2022-01-09 14:48:56 +0300

    Delete "give role" mutation

---
+++ graph/resolver/delete_role_mutation.go
@@ -0,0 +1,39 @@
+package resolver
+
+// This file will be automatically regenerated based on the schema, any resolver implementations
+// will be copied through when generating and any unknown code will be moved to the end.
+
+import (
+	"context"
+	"github.com/saime-0/http-cute-chat/internal/resp"
+	"github.com/saime-0/http-cute-chat/internal/rules"
+
+	"github.com/saime-0/http-cute-chat/graph/model"
+)
+
+func (r *mutationResolver) DeleteRole(ctx context.Context, roleID int) (model.MutationResult, error) {
+	node := r.Piper.CreateNode("mutationResolver > DeleteRole [rid:", roleID, "]")
+	defer node.Kill()
+
+	var (
+		clientID = ctx.Value(rules.UserIDFromToken).(int)
+		chatID   int
+	)
+
+	if node.GetChatIDByRole(roleID, &chatID) ||
+		node.CanCreateRole(clientID, chatID) {
+		return node.Err, nil
+	}
+
+	eventReadyRole, err := r.Services.Repos.Chats.DeleteRole(roleID)
+	if err != nil {
+		return resp.Error(resp.ErrInternalServerError, "не удалось удалить роль"), nil
+	}
+
+	r.Services.Subix.NotifyChatMembers(
+		[]int{chatID},
+		eventReadyRole,
+	)
+
+	return resp.Success("роль удалена"), nil
+}
