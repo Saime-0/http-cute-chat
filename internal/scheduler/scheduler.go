6f62dfebc0baf7a0ce766f334e28d1a262b8b19e
Saime Dejuwyu <acc.saime.d@gmail.com>
2022-01-11 19:06:15 +0300

    Add task scheduler

---
+++ internal/scheduler/scheduler.go
@@ -0,0 +1,67 @@
+package scheduler
+
+import (
+	"sync"
+	"time"
+)
+
+type SheduleStatus int8
+
+const (
+	RUNNING SheduleStatus = iota
+	IDLE
+)
+
+type Scheduler struct {
+	mu     *sync.Mutex
+	root   *Task
+	Status SheduleStatus
+	Ch     chan SheduleStatus
+}
+
+func NewScheduler() *Scheduler {
+	s := &Scheduler{
+		mu: new(sync.Mutex),
+		root: &Task{
+			next: emptyTask(),
+		},
+		Status: IDLE,
+		Ch:     make(chan SheduleStatus),
+	}
+	go s.proccess()
+	<-s.Ch
+	return s
+}
+
+func (s *Scheduler) Start() {
+	s.Status = RUNNING
+	select {
+	case s.Ch <- RUNNING:
+	}
+
+}
+func (s *Scheduler) Stop() {
+	s.Status = IDLE
+	select {
+	case s.Ch <- IDLE:
+	}
+}
+
+func (s *Scheduler) proccess() {
+	s.Ch <- RUNNING
+	for {
+		for s.Status == RUNNING {
+
+			if *s.root.next == nil {
+				s.Status = IDLE
+				break
+			} else if (*s.root.next).executeAt <= time.Now().Unix() {
+				s.runNextTask()
+				continue
+			}
+			time.Sleep(time.Second)
+		}
+
+		<-s.Ch
+	}
+}
---
+++ internal/scheduler/task.go
@@ -0,0 +1,83 @@
+package scheduler
+
+import (
+	"errors"
+	"time"
+)
+
+type execTaskFn func()
+
+type Task struct {
+	sch       *Scheduler
+	executeAt int64
+	taskFunc  execTaskFn
+	next      **Task
+}
+
+func emptyTask() **Task {
+	var x *Task
+	return &x
+}
+
+func (s *Scheduler) RunTask(task **Task) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if task != nil && *task != nil {
+		return errors.New("task not found")
+	}
+	go (*task).taskFunc()
+	*task = *(*task).next
+	return nil
+}
+
+func (s *Scheduler) runNextTask() {
+	task := s.root.next
+	go (*task).taskFunc()
+
+	s.mu.Lock()
+	*task = *(*task).next
+	s.mu.Unlock()
+}
+
+func (s *Scheduler) AddTask(taskFn execTaskFn, executeAt int64) (newTask *Task, err error) {
+	if executeAt <= time.Now().Unix() {
+		return nil, errors.New("not valid executeAt")
+	}
+
+	newTask = &Task{
+		sch:       s,
+		executeAt: executeAt,
+		taskFunc:  taskFn,
+		next:      emptyTask(),
+	}
+
+	task := s.root
+	s.mu.Lock()
+
+	for *task.next != nil {
+		if task.executeAt < newTask.executeAt && (*task.next).executeAt >= newTask.executeAt {
+			newTask.next = task.next
+			break
+		}
+		task = *task.next
+	}
+
+	task.next = &newTask
+	s.mu.Unlock()
+
+	if task == s.root {
+		s.Start()
+	}
+
+	return
+}
+
+func (s *Scheduler) DropTask(task **Task) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if task != nil && *task != nil {
+		return errors.New("task not found")
+	}
+	*task = *(*task).next
+	return nil
+}
